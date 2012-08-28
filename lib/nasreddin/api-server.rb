ff16a98ba78b1e8e0816d75979d05ef3213d3a71
Enrico Rubboli <rubboli@gmail.com>
2012-08-28 15:08:39 +0200

    Nasreddin::APIServer refactoring

--- lib/nasreddin/api-server.rb
+++ lib/nasreddin/api-server.rb
@@ -2,10 +2,31 @@ require 'stringio'
 require 'uri'
 
 module Nasreddin
+
   class APIServer
+
     def initialize(app, options = {})
       @app = app
-      @default_env = {
+      @threads = []
+      if options[:resources]
+         options[:resources].each do |resource|
+            @threads << Thread.new { Nasreddin::APIServerResource.new(resource).run }
+        end
+      else
+        $stderr.puts "WARNING: Nasreddin::APIServer is being used without any resources specified!"
+      end
+      @threads.map(&:join)
+    end
+
+    def call(env)
+      @app.call(env)
+    end
+
+  end
+
+  class APIServerResource 
+
+    DEFAULT_ENV = {
         'rack.errors'       => $stderr,
         'rack.input'        => StringIO.new,
         'rack.version'      => [1, 1],
@@ -14,52 +35,50 @@ module Nasreddin
         'rack.run_once'     => false,
         'HTTP_ACCEPT'       => 'application/json',
         'HTTP_USER_AGENT'   => 'NasreddinAPI'
-      }
+    }
 
-      if options[:resources]
-        options[:resources].each do |resource|
-          Thread.new do
-            queue = TorqueBox::Messaging::Queue.start("/queues/#{resource}", durable: false)
-            begin
-              loop do
-                queue.receive_and_publish do |msg|
-                  begin
-                    env = @default_env.clone
-                    env['rack.url_scheme'] = (msg.delete(:secure) ? 'https' : 'http')
-                    method = msg.delete(:method) || 'GET'
-                    env['REQUEST_METHOD'] = method.to_s.upcase
-                    env['QUERY_STRING'] = queryize(msg.delete(:params))
-                    env['PATH_INFO'] = "#{options[:route_prefix]}/#{resource}/#{msg.delete(:id)}/#{msg.delete(:path)}"
+    def initialize(resource)
+      @resource = resource
+      @queue = TorqueBox::Messaging::Queue.start("/queues/#{@resource}", durable: false)
+    end
 
-                    env.merge!(msg)
-                    status, headers, body = @app.call(env)
-
-                    resp = ''
-                    body.each { |d| resp += d.to_s }
-                    body.close
-
-                  rescue Exception => err
-                    resp = "#{err.message}\n\n#{err.backtrace.join("\n")}"
-                  end
-
-                  [status, headers, resp]
-                end
-              end
-            ensure
-              queue.stop
-            end
+    def run
+      begin
+        loop do
+          @queue.receive_and_publish do |msg|
+              handle_message(msg)
           end
         end
-      else
-        $stderr.puts "WARNING: Nasreddin::APIServer is being used without any resources specified!"
+      ensure
+        @queue.stop
       end
     end
 
-    def call(env)
-      @app.call(env)
+    private
+
+    def handle_message(msg)
+      begin
+        env = DEFAULT_ENV.clone
+        env['rack.url_scheme'] = (msg.delete(:secure) ? 'https' : 'http')
+        method = msg.delete(:method) || 'GET'
+        env['REQUEST_METHOD'] = method.to_s.upcase
+        env['QUERY_STRING'] = queryize(msg.delete(:params))
+        env['PATH_INFO'] = "#{options[:route_prefix]}/#{resource}/#{msg.delete(:id)}/#{msg.delete(:path)}"
+
+        env.merge!(msg)
+        status, headers, body = @app.call(env)
+
+        resp = ''
+        body.each { |d| resp += d.to_s }
+        body.close
+
+      rescue Exception => err
+        resp = "#{err.message}\n\n#{err.backtrace.join("\n")}"
+      end
+
+      [status, headers, resp]
     end
 
-    private
     def queryize(params = {})
       params.map do |key, value|
         if value.is_a? Array
@@ -70,4 +89,5 @@ module Nasreddin
       end.join('&') unless params.nil?
     end
   end
+
 end
