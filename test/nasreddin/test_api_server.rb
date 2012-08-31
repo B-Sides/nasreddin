7eee9f9e1ee3cb8a5471672773c22154a85b53d2
Enrico Rubboli <rubboli@gmail.com>
2012-08-31 16:15:51 +0200

    add specs

--- test/nasreddin/test_api_server.rb
+++ test/nasreddin/test_api_server.rb
@@ -3,22 +3,44 @@ require 'stringio'
 
 describe Nasreddin::APIServerResource do
 
+    let(:resp) { [200,{}, StringIO.new('bar') ] }
+    let(:resource_stubbed) { Nasreddin::APIServerResource.new('prefix','foo',stub()) }
+
     it "should have a DEFAULT_ENV hash" do
       Nasreddin::APIServerResource::DEFAULT_ENV.class.should.equal Hash
     end
 
-    describe ".receive_and_publish" do
-        let(:resp) { [200,{}, StringIO.new('bar') ] }
-        it "should return a status, headers and resp" do
-            app = stub(:call => resp)
-            resource = Nasreddin::APIServerResource.new("prefix","foo",app)
-            resource.expects(:env).returns(nil)
-            result =  resource.process_incoming_message(nil)
-            # this fails, I suppose its a shoulda bug
-            # result.should.equal [200,{},"bar"]
-            result[0].should.equal 200
-            result[2].should.equal "bar"
-        end
+    it ".process_incoming_message should return a status, headers and resp" do
+        app = stub(:call => resp)
+        resource = Nasreddin::APIServerResource.new("prefix","foo",app)
+        resource.expects(:env).returns(nil)
+        result =  resource.process_incoming_message(nil)
+        # this fails, I suppose its a shoulda bug
+        # result.should.equal [200,{},"bar"]
+        result[0].should.equal 200
+        result[2].should.equal "bar"
     end
 
+    it "should connect to hornetq only once" do
+        TorqueBox::Messaging::Queue.expects(:start).once.returns(stub())
+        2.times { resource_stubbed.queue }
+    end
+
+    it "build an env Hash" do
+        msg= {
+            secure: true,
+            method: 'post',
+            params:  { key: 'value'},
+            id: 123,
+            path: 'path',
+            other: 'value'
+        }
+        env= resource_stubbed.env(msg)
+        env['rack.url_scheme'].should.equal 'https'
+        env['REQUEST_METHOD'].should.equal 'POST'
+        env['QUERY_STRING'].should.equal 'key=value'
+        env['PATH_INFO'].should.equal 'prefix/foo/123/path'
+        env[:other].should.equal 'value'
+
+    end
 end
